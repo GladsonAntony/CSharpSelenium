794a9b3a158eb40dfb0ccead24d4318def809249
Gladson Antony <GladsonAntony91@gmail.com>
2023-04-03 10:36:58 +0530

    Added App.Config File to Fetch the Browser Dynamically

--- Utilities/Base.cs
+++ Utilities/Base.cs
@@ -1,10 +1,13 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace CSharpSeleniumFramework.Utilities
 {
@@ -15,11 +18,30 @@ namespace CSharpSeleniumFramework.Utilities
         [SetUp]
         public void SetupBrowser()
         {
-            driver = new EdgeDriver();
+            String ConfigBrowserName = ConfigurationManager.AppSettings["Browser"];
+            InitBrowser(ConfigBrowserName);
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Url = "https://rahulshettyacademy.com/seleniumPractise/#/offers";
             TestContext.WriteLine("Title of the Webpage " + driver.Title);
+        }
+
+        public void InitBrowser(String BrowserName)
+        {
+            switch (BrowserName.ToLower()) 
+            {
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+            }
         }
 
         [TearDown]
